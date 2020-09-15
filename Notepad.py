788abbf06dff737a2f8f26f0397192b50f0e873f
githsem <53379599+githsem@users.noreply.github.com>
2020-09-16 00:38:21 +0200

    Update Notepad.py

--- Notepad.py
+++ Notepad.py
@@ -13,7 +13,37 @@ class Notepad(QWidget):
     def init_ui(self):
         self.yazi_alani = QTextEdit()
         self.temizle = QPushButton("Temizle")
+        self.ac = QPushButton("Ac")
+        self.kaydet = QPushButton("Kaydet")
 
+        h_box = QHBoxLayout()
+        h_box.addWidget(self.temizle)
+        h_box.addWidget(self.ac)
+        h_box.addWidget(self.kaydet)
+
+        v_box = QVBoxLayout()
+        v_box.addWidget(self.yazi_alani)
+        v_box.addLayout(h_box)
+
+        self.setLayout(v_box)
+        self.setWindowTitle("NotePad")
+        self.temizle.clicked.connect(self.yaziyi_temizle)
+        self.ac.clicked.connect(self.dosya_ac)
+        self.kaydet.clicked.connect(self.dosya_kaydet)
+        self.show()
+
+    def yaziyi_temizle(self):
+        self.yazi_alani.clear()
+
+    def dosya_ac(self):
+        dosya_ismi = QFileDialog.getOpenFileName(self, "Dosya Ac", os.getenv("HOME"))
+        with open(dosya_ismi[0], "r") as file:
+            self.yazi_alani.setText(file.read())
+
+    def dosya_kaydet(self):
+        dosya_ismi = QFileDialog.getSaveFileName(self, "Dosya Kaydet", os.getenv("HOME"))
+        with open(dosya_ismi[0], "w") as file:
+            file.write(self.yazi_alani.toPlainText())
 
 
 app = QApplication(sys.argv)
