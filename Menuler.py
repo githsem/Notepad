0c2147ab2f83b078bfc4c59020c3b323a3e09f3d
githsem <53379599+githsem@users.noreply.github.com>
2020-09-16 01:19:28 +0200

    Update Menuler.py

--- Menuler.py
+++ Menuler.py
@@ -1,6 +1,6 @@
 import sys
 
-from  PyQt5.QtWidgets import QApplication, QAction, qApp, QMainWindow
+from PyQt5.QtWidgets import QApplication, QAction, qApp, QMainWindow
 
 class Menu(QMainWindow):
     def __init__(self):
