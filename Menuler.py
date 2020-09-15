a97b7313b84a64120ab41c5a7a171efc4a7f1668
githsem <53379599+githsem@users.noreply.github.com>
2020-09-16 00:58:38 +0200

    Create Menuler.py

---
+++ Menuler.py
@@ -0,0 +1,20 @@
+import sys
+
+from  PyQt5.QtWidgets import QApplication, QAction, qApp, QMainWindow
+
+class Menu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        menubar = self.menuBar()
+
+        dosya = menubar.addMenu("Dosya")
+        duzenle = menubar.addMenu("Duzenle")
+
+        self.setWindowTitle("Menuler")
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+menu = Menu()
+sys.exit(app.exec_())
